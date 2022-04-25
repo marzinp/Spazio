2af87009928bdd9f907b0fb1d117b028ecf43469
marzinp <marzinp@free.fr>
2022-04-25 23:03:59 +0200

    Update O.js

--- O.js
+++ O.js
@@ -17,14 +17,14 @@ class O {
 		rotate(this.angle);
     stroke(this.colB);
     fill(this.colB);
-    ellipse(ech*40*sin(this.sens), ech*40*cos(this.sens), ech*65, ech*65);
+    ellipse(ech*40, ech*40, ech*65, ech*65);
     noFill();
     stroke(this.colA);
     strokeWeight(ech*4);
     ellipse(0, 0, ech*65, ech*65);
     pop();
 		this.start++;
-			if(this.start>this.delay && this.angle%(.25*PI)<.1){
+			if(this.start>this.delay && this.angle%(.5*PI)<.1){
 				this.start=0;
 				this.delay=random(50,300);
 				this.playing=!this.playing;
